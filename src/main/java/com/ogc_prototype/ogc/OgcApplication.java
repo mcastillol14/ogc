cc1fec052a2c7a031f99fce63cf913226f0d0120
maka <castillolopezmario96@gmail.com>
2026-05-17 20:57:00 +0200

    Initialize

---
+++ src/main/java/com/ogc_prototype/ogc/OgcApplication.java
@@ -0,0 +1,13 @@
+package com.ogc_prototype.ogc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OgcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OgcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ogc_prototype/ogc/OgcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ogc_prototype.ogc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OgcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
